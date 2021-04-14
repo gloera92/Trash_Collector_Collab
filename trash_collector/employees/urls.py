cf2f933055187a321aa285c227b120096df83d7b
Brady <bradyrcotton@gmail.com>
2021-04-14 12:54:02 -0500

    added create() to add new employee object

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -7,5 +7,6 @@ from . import views
 app_name = "employees"
 urlpatterns = [
     path('', views.index, name="index"),
-    path('todays_pickup', views.todays_pickup, name='todays_pickup')
+    path('new/', views.create, name='create_new_employee'),
+    path('todays_pickup/', views.todays_pickup, name='todays_pickup')
 ]
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import render,reverse
 from django.apps import apps
-
+from .models import Employees
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
@@ -10,17 +10,33 @@ from django.apps import apps
 def index(request):
     # Get the Customer model from the other app, it can now be used to query the db
     Customer = apps.get_model('customers.Customer')
-    return render(request, 'employees/index.html')
+    user = request.user
+    if Employees.objects.filter(user=request.user).exists():
+        print(user)
+        return render(request, 'employees/index.html')
+
+    else:
+        return render(request, 'employees/create.html')
 
 
 def todays_pickup(request):
-    Customer = apps.get_model('customers.Customer')
-    all_customers = Customer.objects.all()
+    if request.method == "GET":
 
-    employees_customers = all_customers.filter(pickup_date= all_customers)
-    if employees_customers == 'Monday':
-        print(Customer.name)
+        Customer = apps.get_model('customers.Customer')
+        employee = Employees.objects.get(pk=1)
+        context = {
+            'employee': employee
+        }
+    return render(request, 'employees/todays_pickup.html', context)
 
+
+def create(request):
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        zip_code = request.POST.get('zip_code')
+        new_employee = Employees(name=name, user=request.user, zip_code=zip_code)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
     else:
-        return render(request, 'employees/todays_pickup.html')
-
+        return render(request, 'employees/create.html')
